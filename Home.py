c5c3250071c5a893c2b29fbfe001f3e0bc0a227b
oussama-1997-hub <57638718+oussama-1997-hub@users.noreply.github.com>
2025-09-22 11:25:07 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,36 +14,5 @@ with st.form("patient_form"):
         unsafe_allow_html=True
     )
 
-    key_inputs = {}
 
-    # Row 1: Numeric
-    c1, c2 = st.columns(2)
-    key_inputs['Age'] = c1.number_input(
-        "Age (years)",
-        min_value=0, max_value=120,
-        value=int(df['Age'].mean())
-    )
-    key_inputs['BMI_start_PD'] = c2.number_input(
-        "BMI at Start of PD",
-        value=float(df['BMI_start_PD'].mean())
-    )
-
-    # Row 2: Numeric
-    c1, c2 = st.columns(2)
-    key_inputs['Initial_RRF '] = c1.number_input(
-        "Initial RRF",
-        value=float(df['Initial_RRF '].mean())
-    )
-    key_inputs['Initial_albumin'] = c2.number_input(
-        "Initial Albumin",
-        value=float(df['Initial_albumin'].mean())
-    )
-
-    # Row 3: Numeric & Categorical dropdown for Germ
-    c1, c2 = st.columns(2)
-    key_inputs['Nbre_peritonitis'] = c1.number_input(
-        "Number of Peritonitis Episodes",
-        min_value=0,
-        value=int(df['Nbre_peritonitis'].mean())
-    )
-    submitted = st.form_submit_button("🔍 Predict")
+submitted = st.form_submit_button("🔍 Predict")
